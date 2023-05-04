2dac904dd7e466c828dc919f43e460bb72c4db77
Johanna <j.gendler.g@gmail.com>
2023-05-03 21:07:57 -0300

    fix auth

--- controller/requestController.js
+++ controller/requestController.js
@@ -1,11 +1,11 @@
-import { findUser } from "../service/userService";
-import { add } from "../service/requestService";
+import { findUser } from "../service/userService.js";
+import { add } from "../service/requestService.js";
 
-async function add(body, userid) {
+async function addRequest(body, userid) {
   await findUser(userid);
   
   const request = await add(body, userid);
   return request;
 }
 
-export { add };
+export { addRequest };
