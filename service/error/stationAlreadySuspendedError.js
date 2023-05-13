5da34c24316f8f34f2404ab50571047cfad1c42a
JuanFerreyros <juansferreyros@gmail.com>
2023-05-13 18:52:10 -0300

    Pull request Solved

--- service/error/stationAlreadySuspendedError.js
+++
@@ -1,8 +0,0 @@
-class StationAlreadySuspendedError extends Error {
-    constructor(stationId) {
-      super(`Station with ID ${stationId} is already suspended`);
-      this.name = "StationAlreadySuspendedError";
-    }
-  }
-  
-  export { StationAlreadySuspendedError };
--- service/error/userNotAuthorizedError.js
+++
@@ -1,8 +0,0 @@
-class UserNotAuthorizedError extends Error {
-    constructor(userId) {
-      super(`User Id ${userId} is not authorized`);
-      this.name = "UserNotAuthorizedError";
-    }
-  }
-  
-  export { UserNotAuthorizedError };
