829ac59a6f919e0a87aebf488d8f22589acf3028
PseudoEmpirico <facundopradells@gmail.com>
2023-05-13 20:52:09 -0300

    stations by user id

--- model/filterStation.js
+++ model/filterStation.js
@@ -1,11 +1,12 @@
 class FilterStation {
-  constructor(pageSize, page, name, serialNumber, status, date) {
+  constructor(pageSize, page, name, serialNumber, status, date, userId) {
     this.pageSize = pageSize;
     this.page = page;
     this.name = name;
     this.serialNumber = serialNumber;
     this.status = status;
     this.date = date;
+    this.userId = userId;
   }
 }
 
--- service/stationService.js
+++ service/stationService.js
@@ -1,21 +1,27 @@
-import { findBySerialNumber, create, findById,findAll, update } from "../data/stationData.js";
+import {
+  findBySerialNumber,
+  create,
+  findById,
+  findAll,
+  update,
+} from "../data/stationData.js";
 import { Rol } from "../model/enum/rol.js";
-import {StationStatus} from "../model/enum/stationStatus.js"
-import {StationNotFoundError} from "./error/stationNotFoundError.js"
+import { StationStatus } from "../model/enum/stationStatus.js";
+import { StationNotFoundError } from "./error/stationNotFoundError.js";
 
 async function exists(serialNumber) {
   const station = await findBySerialNumber(serialNumber);
   return station != null && station.status != StationStatus.INACTIVE;
 }
 
-async function add(request, userId) {
+async function add(station, userId) {
   const fullStation = {
-    serial_number: request.serial_number,
-    name: request.name,
-    longitud: request.longitud,
-    latitud: request.latitud,
-    brand: request.brand,
-    model: request.model,
+    serial_number: station.serial_number,
+    name: station.name,
+    longitud: station.longitud,
+    latitud: station.latitud,
+    brand: station.brand,
+    model: station.model,
     status: StationStatus.ACTIVE,
     created_by: parseInt(userId),
     created_at: new Date(),
@@ -25,32 +31,32 @@ async function add(request, userId) {
   return await findById(newStation.insertedId);
 }
 
-async function suspend(userId, stationId, rol){
-    const stationToSuspend = await findById(stationId);
+async function suspend(userId, stationId, rol) {
+  const stationToSuspend = await findById(stationId);
 
-    if (!stationToSuspend) {
-        throw new StationNotFoundError(stationId);  
-    }
+  if (!stationToSuspend) {
+    throw new StationNotFoundError(stationId);
+  }
 
-    if (stationToSuspend.created_by !== userId && rol === Rol.USER) {
-        throw new StationNotFoundError(stationId);
-    }
+  if (stationToSuspend.created_by !== userId && rol === Rol.USER) {
+    throw new StationNotFoundError(stationId);
+  }
 
-    if (stationToSuspend.status !== StationStatus.ACTIVE) {
-        throw new StationNotFoundError(stationId);
-    }
-    
-    stationToSuspend.status = StationStatus.INACTIVE;
-    await update(stationId, stationToSuspend);
+  if (stationToSuspend.status !== StationStatus.ACTIVE) {
+    throw new StationNotFoundError(stationId);
+  }
 
-    const suspendedStation = await findById(stationId);
+  stationToSuspend.status = StationStatus.INACTIVE;
+  await update(stationId, stationToSuspend);
 
-    return suspendedStation;
+  const suspendedStation = await findById(stationId);
+
+  return suspendedStation;
 }
 
-async function get(filterRequests) {
-    const request = await findAll(filterRequests);
-    return request;
-  }
+async function get(filterStation) {
+  const stations = await findAll(filterStation);
+  return stations;
+}
 
 export { exists, add, suspend, get };
